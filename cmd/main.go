0df82c70b2926bc6c47d81ef0966e3d651491eba
LeadNess <leadness@mail.ru>
2020-06-09 17:42:38 +0300

    Update api/vk
    Add GetGroupsPosts method for getting posts from list of groups using VK Api execute method

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"../pkg/api"
+	"fmt"
 	"log"
+	"time"
 	"os"
 )
 
@@ -13,12 +14,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	wall, err := session.WallGet("meduzaproject")
+	groupsDomains := []string{"meduzaproject", "ria", "kommersant_ru", "tj", "rbc"}
+	groupsWall, err := session.GetGroupsPosts(groupsDomains, 3)
+
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		for _, post := range wall.Items {
-			fmt.Printf("%s\n%s\n\n", post.Attachments[0].Link.Title, post.Attachments[0].Link.Description)
+		for domain, wall := range groupsWall {
+			fmt.Printf("Group: %s\n\n", domain)
+			for _, post := range wall.Items {
+				if len(post.Attachments) != 0 &&
+					post.Attachments[0].Link.Title != "" &&
+					post.Attachments[0].Link.Description != "" {
+					fmt.Printf("Title: %s\n\nDescription: %s\n\nDate: %s\nLikes: %d\nViews: %d\nComments: %d\n\n\n",
+						post.Attachments[0].Link.Title, post.Attachments[0].Link.Description,
+						time.Unix(post.Date, 0), post.Likes.Count, post.Views.Count, post.Comments.Count)
+				}
+			}
 		}
 	}
 }
--- pkg/api/vk.go
+++ pkg/api/vk.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"github.com/go-vk-api/vk"
+	"strconv"
 )
 
 type VKAPi struct {
@@ -24,4 +26,31 @@ func (a *VKAPi) WallGet(domain string) (VKWall, error) {
 		"count": 100,
 	}, &wall)
 	return wall, err
+}
+
+func (a *VKAPi) GetGroupsPosts(domains []string, postsCount int) (map[string]VKWall, error) {
+	groupsDomains := make([]string, len(domains))
+	for i, str := range domains {
+		groupsDomains[i] = fmt.Sprintf("%s", strconv.Quote(str))
+		if i != len(domains) - 1{
+			groupsDomains[i] += ","
+		}
+	}
+	var response []VKWall
+	code := "var domains = %s;" +
+			"var res = [];" +
+			"var i = 0;" +
+			"while (i < domains.length) {" +
+				"var posts = API.wall.get({domain: domains[i], count: %d});" +
+				"res.push(posts);" +
+				"i = i + 1; }" +
+			"return res;"
+	err := a.api.CallMethod("execute", vk.RequestParams{
+		"code": fmt.Sprintf(code, groupsDomains, postsCount),
+	}, &response)
+	wallMap := make(map[string]VKWall, len(domains))
+	for i, wall := range response {
+		wallMap[domains[i]] = wall
+	}
+	return wallMap, err
 }
